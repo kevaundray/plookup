8303fa6e04486b8cb71a5bc5a358581440c4a24a
Kevaundray Wedderburn <kev@dusk.network>
2020-03-27 20:30:43 +0000

    Add wrapper method in kzg10 to compute witness and verify a proof

--- src/kzg10.rs
+++ src/kzg10.rs
@@ -1,7 +1,7 @@
 use algebra::bls12_381::Fr;
 use algebra::Bls12_381;
 use ff_fft::DensePolynomial as Polynomial;
-use poly_commit::kzg10::{Commitment, Powers, UniversalParams, VerifierKey, KZG10};
+use poly_commit::kzg10::{Commitment, Powers, Proof, UniversalParams, VerifierKey, KZG10};
 use rand_chacha::ChaChaRng;
 use rand_core::SeedableRng;
 // Modification of https://github.com/scipr-lab/poly-commit/blob/master/src/kzg10/mod.rs
@@ -49,4 +49,29 @@ pub fn commit(powers: &Powers<Bls12_381>, p: &Polynomial<Fr>) -> Commitment<Bls1
     let hiding_bound = None;
     let (comm, _) = KZG10::commit(&powers, &p, hiding_bound, None).unwrap();
     comm
+}
+
+// Computes the witness for a set of polynomials evaluated at the same point
+// W(X) = f(x) - f(z) / x-z
+// However, the quotient is invariant under `f(z)`,
+// So we can compute the witness as f(x) / x-z
+pub fn compute_witness(polynomial: &Polynomial<Fr>, point: Fr) -> Polynomial<Fr> {
+    let divisor = Polynomial::from_coefficients_vec(vec![-point, Fr::from(1u8)]);
+
+    polynomial / &divisor
+}
+
+pub fn verify(
+    vk: &VerifierKey<Bls12_381>,
+    commitment_to_poly: &Commitment<Bls12_381>,
+    commitment_to_witness: &Commitment<Bls12_381>,
+    evaluation_point: Fr,
+    value: Fr,
+) -> bool {
+    let proof = Proof {
+        w: commitment_to_witness.0,
+        random_v: Fr::from(0u8),
+    };
+
+    KZG10::check(vk, commitment_to_poly, evaluation_point, value, &proof).unwrap()
 }
