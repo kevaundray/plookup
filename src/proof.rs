d6789c37d45e7cf3bfb7dcd6f9f4eb9d8539451c
Kevaundray Wedderburn <kev@dusk.network>
2020-03-30 22:12:53 +0100

    Allow verifier to compute the quotient evaluation point from the prover messages

--- src/proof.rs
+++ src/proof.rs
@@ -4,9 +4,9 @@ use crate::multiset;
 use crate::transcript::TranscriptProtocol;
 use algebra::bls12_381::Fr;
 use algebra::Bls12_381;
+use ff_fft::EvaluationDomain;
 use poly_commit::kzg10::Commitment;
 use poly_commit::kzg10::VerifierKey;
-
 // Opening Proof consists of three components:
 // - The commitment to the polynomial, which may be `None` if we have already included the commitment
 // - The Witness for an opening `z`
@@ -58,4 +58,64 @@ pub struct MultiSetEqualityProof {
     pub z_omega_proof: OpeningProof,
     // XXX: We include also the evaluations for t since the verifier cannot know pre-prover, what it is.
     // Surely, we can take advantage of the fact that the verifier has the table in the preprocessing stage?
+}
+
+impl MultiSetEqualityProof {
+    /// Computes the quotient evaluation from the prover messages
+    fn compute_quotient_evaluation(
+        &self,
+        beta: &Fr,
+        gamma: &Fr,
+        evaluation_challenge: &Fr,
+        domain: &EvaluationDomain<Fr>,
+    ) -> Fr {
+        // g^{n+1}
+        let last_element = domain.elements().last().unwrap();
+
+        let lagrange_evaluations = domain.evaluate_all_lagrange_coefficients(*evaluation_challenge);
+        // L_1(Z);
+        let l1_z = lagrange_evaluations[0];
+        // L_{n+1}(Z);
+        let ln_plus_1_z = lagrange_evaluations[domain.size() - 1];
+
+        // Z_H(Z)
+        let v_h = domain.evaluate_vanishing_polynomial(*evaluation_challenge);
+
+        let beta_one = Fr::from(1u8) + beta;
+        let gamma_beta_one = (Fr::from(1u8) + beta) * gamma;
+
+        // L_1(X) [ Z(X) -1]
+        let a = { (self.z_proof.evaluation - Fr::from(1u8)) * l1_z };
+
+        // x-g^{n+1} * Z(X)(1+beta) * (gamma + f(x)) (gamma(1+beta) + t(x) + beta * t(Xg))
+        let b = {
+            let b_0 = *evaluation_challenge - last_element;
+            let b_1 = self.z_proof.evaluation * beta_one;
+            let b_2 = self.f_proof.evaluation + gamma;
+            let b_3 =
+                gamma_beta_one + self.t_proof.evaluation + (self.t_omega_proof.evaluation * beta);
+            b_0 * b_1 * b_2 * b_3
+        };
+        // x-g^{n+1} * Z(Xg)[(gamma(1+beta) + h_1(X) + beta * h_1(Xg)][(gamma(1+beta) + h_2(X) + beta * h_2(Xg)]
+        let c = {
+            let c_0 = (*evaluation_challenge - last_element) * self.z_omega_proof.evaluation;
+
+            let c_1 = gamma_beta_one
+                + self.h_1_proof.evaluation
+                + (self.h_1_omega_proof.evaluation * beta);
+
+            let c_2 = gamma_beta_one
+                + self.h_2_proof.evaluation
+                + (self.h_2_omega_proof.evaluation * beta);
+
+            c_0 * c_1 * c_2
+        };
+
+        // L_{n+1}(X)[h_1(X) - h_2(Xg)]
+        let d = ln_plus_1_z * (self.h_1_proof.evaluation - self.h_2_omega_proof.evaluation);
+        // L_{n+1}(X)[Z(X) - 1]
+        let e = (self.z_proof.evaluation - Fr::from(1u8)) * ln_plus_1_z;
+
+        (a + b - c + d + e) / v_h
+    }
 }
